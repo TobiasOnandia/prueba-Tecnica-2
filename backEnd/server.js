49fb327e061458a6c79c6d0984ec949336cc01f9
Tobias Onandia <tobiasonandia0@gmail.com>
2024-05-23 12:16:09 -0300

    middleware

--- backEnd/server.js
+++ backEnd/server.js
@@ -4,9 +4,9 @@ import cors from 'cors'
 const app = express()
 const PORT = process.env.PORT || 3000
 
-
+app.use(express.json())
 app.use(cors()) //enable cors
-
+app.disable('x-powered-by')
 
 app.get('/',  (req,res) => {
     res.send({message: 'hello worlds'})
@@ -18,12 +18,20 @@ app.get('/items', (req,res) => {
 })
 
 
-
 app.get('/items/:id', (req,res) => {
     res.send("pagina del detalle")
 })
 
-app.post()
+
+app.post('items', (req,res) => {
+    res.status(201).json(req.body)
+})
+
+
+app.use((req,res) => {
+    res.status(404).send('<h1>404</h1>')
+})
+
 
 
 app.listen(PORT, () => {
