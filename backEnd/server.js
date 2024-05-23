a2040082dd0da34f1196ceeced17c442fd49e7b0
Tobias Onandia <tobiasonandia0@gmail.com>
2024-05-23 13:01:23 -0300

    filter by names

--- backEnd/server.js
+++ backEnd/server.js
@@ -10,17 +10,24 @@ app.use(cors()) //enable cors
 app.disable('x-powered-by')
 
 
-app.get('/items', (req,res) => {
+app.get('/api/items', (req,res) => {
+    if(req.query.q){
+        const filteredProducts = products.filter(product => product.title.toLowerCase().includes(req.query.q.toLowerCase()))
+        res.send(filteredProducts)
+        return
+    }
     res.send(products)
 })
 
+
 //filter product by params 
 
-app.get('/items/:id', (req,res) => {
-    console.log(req.params.id)
+app.get('/api/items/:id', (req,res) => {
     const prod = products.filter(product => product.id === Number(req.params.id))
     res.send(prod)
 })
+
+
 
 
 app.post('items', (req,res) => {
